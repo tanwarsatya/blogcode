7d8620be1e6b8ec8eae97e5317684686e498a4fe
Satya Tanwar <satya.tanwar@gmail.com>
2021-11-24 23:21:31 -0500

    updated tf files

---
+++ pod-topology-spread-constraints/iac/azure.tf
@@ -0,0 +1,35 @@
+resource "azurerm_resource_group" "podconstraints" {
+  name     = "podconstraints"
+  location = "East Us"
+}
+
+resource "azurerm_kubernetes_cluster" "aksmultizone" {
+  name                = "aks-multizone"
+  location            = azurerm_resource_group.podconstraints.location
+  resource_group_name = azurerm_resource_group.podconstraints.name
+  dns_prefix          = "aksmultizone"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_B2s"
+    availability_zones = ["1", "2", "3"] 
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aksmultizone.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aksmultizone.kube_config_raw
+  sensitive = true
+}
