dc568ea24a7fedeb28434a986e36261da88cdfc6
Satya Tanwar <satya.tanwar@gmail.com>
2021-11-30 22:49:13 -0500

    updated the scenarios bash scripts

--- pod-topology-spread-constraints/scenarios/scale-different-numbers.sh
+++ pod-topology-spread-constraints/scenarios/scale-different-numbers.sh
@@ -1,4 +1,4 @@
-kubectl --kubeconfig output/config -n=dev scale deployment a-micro --replicas=3
+kubectl --kubeconfig output/config -n=dev scale deployment a-micro --replicas=13
 kubectl --kubeconfig output/config -n=dev scale deployment b-micro --replicas=5
-kubectl --kubeconfig output/config -n=dev scale deployment c-micro --replicas=8
+kubectl --kubeconfig output/config -n=dev scale deployment c-micro --replicas=9
 
--- pod-topology-spread-constraints/scenarios/scale-same-numbers.sh
+++ pod-topology-spread-constraints/scenarios/scale-same-numbers.sh
@@ -1,4 +1,4 @@
-kubectl --kubeconfig output/config -n=dev scale deployment a-micro --replicas=7
-kubectl --kubeconfig output/config -n=dev scale deployment b-micro --replicas=7
-kubectl --kubeconfig output/config -n=dev scale deployment c-micro --replicas=7
+kubectl --kubeconfig output/config -n=dev scale deployment a-micro --replicas=1
+kubectl --kubeconfig output/config -n=dev scale deployment b-micro --replicas=1
+kubectl --kubeconfig output/config -n=dev scale deployment c-micro --replicas=1
 
